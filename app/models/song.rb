649f507db9ac091f24d0ffb9b46e547ff2417adb
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-08-05 16:44:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -21,7 +21,7 @@ class Song < ActiveRecord::Base
 
   def note_contents=(notes)
     notes.each do |content|
-      if content != ""
+      if content.strip != ""
         self.notes.build(content: content)
       end
     end
